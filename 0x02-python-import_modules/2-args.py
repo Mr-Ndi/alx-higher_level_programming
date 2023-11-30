8ed062af5aa57019aad9ca20f147d7a1ca9854dc
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 12:30:03 +0200

    ushaka inka aryama nkayo

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+length = len(sys.argv) - 1
+if length == 0:
+    print("{} arguments.".format(length))
+elif length == 1:
+    print("{} argument:".format(length))
+    print("{}: {}".format(length, sys.argv[length]))
+else:
+    print("{} arguments:".format(length))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
