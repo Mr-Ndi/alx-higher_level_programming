e9a43533e005806b035d9d72ffe730ee467fd255
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 16:21:28 +0200

    tutaniweza

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-import sys
-import calculator_1 as calc
+if "__name__ == __main__":
+    import sys
+    import calculator_1 as calc
 
+    number = len(sys.argv) - 1
 
-number = len(sys.argv) - 1
+    if number != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-if number != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operat = sys.argv[2]
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-operat = sys.argv[2]
+    if operat not in ('+', '-', '*', '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if operat not in ('+', '-', '*', '/'):
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operat == '+':
+        ans = calc.add(a, b)
+    elif operat == '-':
+        ans = calc.sub(a, b)
+    elif operat == '*':
+        ans = calc.mul(a, b)
+    elif operat == '/':
+        ans = calc.div(a, b)
 
-if operat == '+':
-    ans = calc.add(a, b)
-elif operat == '-':
-    ans = calc.sub(a, b)
-elif operat == '*':
-    ans = calc.mul(a, b)
-elif operat == '/':
-    ans = calc.div(a, b)
-
-print("{} {} {} = {}".format(a, operat, b, ans))
+    print("{} {} {} = {}".format(a, operat, b, ans))
