479d3529eaefdcc22812ac8a47c36b9045e77966
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 12:46:00 +0200

    ushaka inka aryama nkayo

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+length = len(sys.argv) - 1
+if length == 0:
+    print(length)
+else:
+    sum = 0
+    for i in range(1, length + 1):
+        sum += int(sys.argv[i])
+    print(sum)
