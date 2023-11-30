10a91d161e34d38a18a7142229563a2dd0bee426
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 12:00:00 +0200

    ushaka inka aryama nkayo

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+
+
+a = 10
+b = 5
+
+summ = cal.add(a, b)
+diff = cal.sub(a, b)
+pro = cal.mul(a, b)
+quo = cal.div(a, b)
+print("{} + {} = {}".format(a, b, summ))
+print("{} - {} = {}".format(a, b, diff))
+print("{} * {} = {}".format(a, b, pro))
+print("{} / {} = {}".format(a, b, quo))
