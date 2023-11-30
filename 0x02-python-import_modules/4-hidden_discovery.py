e3551b818fd1ece1bb98d3f8eaa269f285a17d88
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 17:23:02 +0200

    tutaniweza

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
-#1/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-
+#!/usr/bin/python3
+import hidden_4
+if __name__ == '__main__':
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in names:
+        if not i.startswith("__"):
+            print(i)
