1a3bfbfebd38747fa4468f32becfa0daf2bd18c4
ndiramiye2 <onlythenotes@gmail.com>
2023-11-30 16:07:50 +0200

    tutaniweza

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
+if "__name__ == __main__":
+    import sys
 
-length = len(sys.argv) - 1
-if length == 0:
-    print(length)
-else:
-    sum = 0
-    for i in range(1, length + 1):
-        sum += int(sys.argv[i])
-    print(sum)
+    length = len(sys.argv) - 1
+    if length == 0:
+        print(length)
+    else:
+        sum = 0
+        for i in range(1, length + 1):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
