b88f424a9c1e1a0dbad8445b22d846e6f08667d4
ndiramiye2 <onlythenotes@gmail.com>
2023-12-02 20:48:49 +0200

    I'm making some changes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for ele in my_list:
-        ele *= -1
+    ingano = len(my_list) - 1
+    for ele in range(ingano, -1, -1):
         print("{:d}".format(my_list[ele]))
