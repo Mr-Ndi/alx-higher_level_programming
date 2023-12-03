196459f267e6ff092cc153a32a16491dff2e9669
ndiramiye2 <onlythenotes@gmail.com>
2023-12-03 14:35:23 +0200

    I'm making some changes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,11 +3,10 @@ def divisible_by_2(my_list=[]):
     if my_list == []:
         return None
     else:
-        ans = my_list.copy()
-        leng = len(my_list)
-        for elem in range(leng - 1):
-            if my_list[elem] % 2 == 0:
-                ans[elem] = True
+        ans = []
+        for elem in my_list:
+            if elem % 2 == 0:
+                ans.append(True)
             else:
-                ans[elem] = False
+                ans.append(False)
         return ans
