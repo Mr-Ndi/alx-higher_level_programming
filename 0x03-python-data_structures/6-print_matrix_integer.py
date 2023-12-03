3cd8144943cf86e04e82c53c0db861efa3aac130
ndiramiye2 <onlythenotes@gmail.com>
2023-12-03 11:45:04 +0200

    I'm making some changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for k in i:
-            if k == i [-1]:
+            if k == i[-1]:
                 print("{:d}".format(k), end='')
             else:
                 print("{:d}".format(k), end=' ')
