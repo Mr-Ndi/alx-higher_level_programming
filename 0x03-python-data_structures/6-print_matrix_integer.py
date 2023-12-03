3fabb5df57df7404eed0058cbd4d1a45096ef190
ndiramiye2 <onlythenotes@gmail.com>
2023-12-03 11:35:28 +0200

    I'm making some changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for k in i:
-            print("{:d}".format(k),end=' ')
+            print("{:d}".format(k), end=' ')
         print()
