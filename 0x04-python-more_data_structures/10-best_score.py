733ddcb9590c531897dbeada574ee57448c48cd6
ndiramiye2 <onlythenotes@gmail.com>
2023-12-07 16:37:25 +0200

    Some changes are here

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,10 @@ def best_score(a_dictionary):
 
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    init = list(a_dictionary)
-
-    begin = init[0]
-    for ii in init:
-        if ii > begin:
-            begin = ii
-    return begin
+    else:
+        init = list(a_dictionary)
+        begin = init[0]
+        for ii in init:
+            if ii > begin:
+                begin = ii
+        return begin
