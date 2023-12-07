ed05e7ef89583bd8a4fcfb249b0c90915598ff30
ndiramiye2 <onlythenotes@gmail.com>
2023-12-07 12:24:13 +0200

    Some changes are here

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+
+    sign = a_dictionary.get(key)
+    if sign is not None:
+        a_dictionary[key] = value
+    return a_dictionary
