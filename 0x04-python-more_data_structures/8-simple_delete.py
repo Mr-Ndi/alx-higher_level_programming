f78e35df4df56bb0b4c71b8d59c9086fab16b057
ndiramiye2 <onlythenotes@gmail.com>
2023-12-07 16:01:30 +0200

    Some changes are here

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
-#!/usr/bin/usr/python3
-def simple_delete(a_dictionary, key=""):
-
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key):
     if key in set(list(a_dictionary)):
         del a_dictionary[str(key)]
     return a_dictionary
