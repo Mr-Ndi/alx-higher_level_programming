2019fa7934879a014642baa883346d7f92d931bf
ndiramiye2 <onlythenotes@gmail.com>
2023-12-19 09:25:19 +0200

    I'm gonna do that

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,5 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
+    except Exception as e:
+        print("Exception: {}".format(e), file = sys.stderr)
         return False
