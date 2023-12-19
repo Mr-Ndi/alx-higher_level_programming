06e6cb998cd0d43de791e4fb3de0da7f82708089
ndiramiye2 <onlythenotes@gmail.com>
2023-12-19 09:20:56 +0200

    I'm gonna do that

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             ans = 0
             print("out of range")
+        finally:
+            pass
         new.append(ans)
     return new
