92662523bf35319c96f8f15334e43a01e195899f
ndiramiye2 <onlythenotes@gmail.com>
2023-12-20 22:09:02 +0200

    I'm gonna do that

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,61 @@
 #!/usr/bin/python3
-"""
-A simple class
+"""Square generation module for Python project 0x06
 """
 
 
 class Square:
-    """Class containing the size attribute"""
+    """Class defined for square generation.
+
+    Args:
+        size (int): length of one side of square
+
+    Attributes:
+        __size (int): length of one side of square
+
+    """
+
     def __init__(self, size=0):
-        """initiator function"""
+        # attribute assigment here engages setters defined below
         self.size = size
 
     @property
     def size(self):
+        """__size getter, setter with same method name
+
+        Returns:
+            __size (int): length of one side, squared
+
+        """
         return self.__size
 
     @size.setter
-    def size(self, size):
-        """setter of Square class"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    def size(self, value):
+        """Args:
+            value (int): length of one side of square
+
+        Attributes:
+            __size (int): length of one side of square
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
-        """compute the area of the square"""
-        return (self.__size)**2
+        """Calulates area of square.
+
+        Attributes:
+            __size (int): length of one side of square
+
+        Returns:
+            area (int): length of one side, squared
+
+        """
+        area = self.__size * self.__size
+        return area
