514f79d58685b62dee8129c8217f3939ed9b6b1d
ndiramiye2 <onlythenotes@gmail.com>
2023-12-20 20:22:41 +0200

    I'm gonna do that

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a class for a simple siquare
+"""
+
+
+class Square:
+    """class definition"""
+    def __init__(self, size=0):
+        """initialisation"""
+
+        self.size = size
+
+    def size(self, value):
+        """initialisation"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def size(self):
+        """returning the size"""
+        return self.__size
+
+    def area(self):
+        """returning the area of the square"""
+        return self.__size ** 2
