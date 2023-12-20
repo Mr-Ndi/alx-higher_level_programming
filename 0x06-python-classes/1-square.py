fc2d763a2aee0fc2762fe68a859213de8080af07
ndiramiye2 <onlythenotes@gmail.com>
2023-12-20 18:13:58 +0200

    I'm gonna do that

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A class for non empty square
+"""
+
+
+class Square:
+    """
+    Definition
+    """
+    def __init__(self, size):
+        """
+        initialisation of a class"""
+        self.__size = size
