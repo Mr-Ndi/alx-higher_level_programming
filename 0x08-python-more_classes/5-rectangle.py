98935097a4fb6180248c73e328e0b8fc78ae1d4f
ndiramiye2 <onlythenotes@gmail.com>
2024-01-04 17:37:03 +0200

    first-class everything

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Setting up the value and width'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Setting up the value and height'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        '''for displaying the area'''
+        return self.__width * self.__height
+
+    def perimeter(self):
+        '''a function for returning the perimeter'''
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        '''function to create a rectangle'''
+        if self.__width == 0 or self.__height == 0:
+            return ('')
+        return '\n'.join(['#' * self.__width] * self.__height)
+
+    def print(self):
+        '''for displaying the created rectangle'''
+        print(str(self))
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
