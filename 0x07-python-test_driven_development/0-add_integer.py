31bf175519917e302049feceb976524beed763e3
ndiramiye2 <onlythenotes@gmail.com>
2024-01-09 07:32:43 +0200

    first-class everything

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ Float arguments are type casted into integers before perfoming the operator
 if b or a is not a number means not integer or not a float type error is raised
 """
 
+
 def add_integer(a, b=98):
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
