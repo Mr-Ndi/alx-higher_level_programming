e9cfd9950e01f4069585e4b09ffcba20b1b43241
ndiramiye2 <onlythenotes@gmail.com>
2024-01-09 16:04:22 +0200

    first-class everything

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='UTF-8') as p:
-            cont = p.read()
-            print(cont)
-    except FileNotFound:
-        print("file not found")
-    except UnicodeDecodeError:
-        print("unable to decode the file")
+    with open(filename, 'r', encoding='UTF-8') as p:
+        cont = p.read()
+        print(cont)
