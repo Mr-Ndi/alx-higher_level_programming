fcdd1bdc0cc1f91f13b505bbe3b13b11486e5b8e
ndiramiye2 <onlythenotes@gmail.com>
2024-01-09 16:12:48 +0200

    first-class everything

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,8 @@
 """the beginig of the project and this
     will focus on the file
 """
+
+
 def read_file(filename=""):
     """ a function to read the file content"""
     with open(filename) as p:
