d0ba560d863207e339ad73c61a7789d88143bb8a
ndiramiye2 <onlythenotes@gmail.com>
2024-01-09 17:33:59 +0200

    first-class everything

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This will display the presentation of json string object
+    only.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ A function to help us with that"""
+    return json.dumps(my_obj)
