257d51b0966f842a53c0c8708d52d1b7d72c3809
ndiramiye2 <onlythenotes@gmail.com>
2024-01-10 12:29:46 +0200

    Nitafanya hivyo peke yangu

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """a function that returns the list
+        of available attributes and methods of an object
+    """
+    print(dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This is the Directory for the tasks of project called 0x0A-python-inheritance
