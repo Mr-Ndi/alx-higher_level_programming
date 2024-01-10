13c255bb052f21d7906c950c950d8deb967d90b8
ndiramiye2 <onlythenotes@gmail.com>
2024-01-10 20:46:08 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,9 +10,9 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialiser that initialise an instance of a class"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
