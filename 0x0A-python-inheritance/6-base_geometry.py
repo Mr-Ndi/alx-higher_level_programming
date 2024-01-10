66b94411d04f022370c8f2a980a0e7a38f8a35f1
ndiramiye2 <onlythenotes@gmail.com>
2024-01-10 16:32:55 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """An Empty class"""
     def area(self):
         """A class that raises an Exception with a message """
-        raise ValueError("area() is not implemented")
+        raise Exception("area() not implemented")
