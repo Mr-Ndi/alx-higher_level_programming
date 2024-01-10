8bf6fd198787936042c364ba513aa4c3cfefa21f
ndiramiye2 <onlythenotes@gmail.com>
2024-01-10 20:59:37 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
-
-
+""" class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """a class that inherted the basegeometry class"""
+    """rectangle with BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialiser that initialise an instance of a class"""
-        self.integer_validator("width", width)
+        """
+        constructor of the classs
+        """
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """A function to imprement the area"""
-        rn = self.__width * self.__height
-        return rn
+        """Areof findinf function of rectangle."""
+        return self.__width * self.__height
 
     def __str__(self):
-        """Returning the following rectangle description"""
-        ans = "[" + str(self.__class__.__name__) + "]"
-        ans += str(self.__width) + "/" + str(self.__height)
-        return ans
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
