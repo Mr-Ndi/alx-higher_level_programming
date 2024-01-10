22db0c61101cf6b7261c5ee80f63bcaf7edf94ac
ndiramiye2 <onlythenotes@gmail.com>
2024-01-10 15:00:32 +0200

    Nitafanya hivyo peke yangu

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""returning True if the object is
+    an instance of the specified False otherwise
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This function that returns True if the object
+        is exactly an instance of the specified class
+        otherwise return False
+    """
+    return isinstance(obj, a_class)
