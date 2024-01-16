8080dff5d1dac44c82ad0c6f2cf8636f605a86a0
ndiramiye2 <onlythenotes@gmail.com>
2024-01-16 19:34:26 +0200

    Nitafanya hivyo peke yangu

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,16 +28,25 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
-    def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts"""
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
+        """Write the JSON serialization of a list of objects to a file.
 
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
