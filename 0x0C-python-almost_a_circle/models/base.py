44fb370279a72b99bc786d21abd8fc2e60aa92d2
ndiramiye2 <onlythenotes@gmail.com>
2024-01-16 17:30:35 +0200

    Nitafanya hivyo peke yangu

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,18 +27,20 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """static method that returns JSON string representation"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-
+    
+    @classmethod
     def save_to_file(cls, list_objs):
         """A class method that writes Json string to a file"""
         if list_objs is None or list_objs == []:
             with open("base.json", "w") as dd:
-                json.domb([], dd)
+                json.domp([], dd)
         else:
             list_dicts = [obj.to_dictionary() for obj in list_objs]
             with open("base.json", "w") as dd:
