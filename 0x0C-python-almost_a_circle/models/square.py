c32cabd9a896cb25eea6b65711a6b114047434cb
ndiramiye2 <onlythenotes@gmail.com>
2024-01-16 12:43:12 +0200

    Nitafanya hivyo peke yangu

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" this is the first class for this project based on
-python overall modeles
-Wwe are lookin for importing and many things about pythn and oop
-"""
-from models.rectangle import Rectangle
+"""Defines a base model class."""
+import json
 
 
 class Base:
-    """ this is the base class that we are gonna use in
-    this overall project about python
+    """Represent the base model.
+
+    Represents the "base" for all other classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ constructor for ID of class Base
-            This will also increment the class attribute nb_of_object
-            when ever new instance is created
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
         """
         if id is not None:
             self.id = id
@@ -23,46 +26,14 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @property
-    def size(self):
-        """Get/set the size of the Square. that setts everything to
-            The value of size as long as height and width is equal"""
-        return self.width
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
 
-    @size.setter
-    def size(self, value):
-        """this one will set the value of height and value of width
-            to the same value since height and width of square is the same
+        Args:
+            list_dictionaries (list): A list of dictionaries.
         """
-        self.width = value
-        self.height = value
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
-
-    def update(self, *args, **kwargs):
-        """ public method that assigns attributes"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) >= 4:
-            self.y = args[3]
-        else:
-            if len(args) == 0:
-                pass
-            for opt, tipo in kwargs.items():
-                setattr(self, opt, tipo)
-
-    def to_dictionary(self):
-        """Public method that returns the dictionary"""
-        return {
-            "id": self.id,
-            "width": self.width,
-            "x": self.x,
-            "y": self.y,
-        }
