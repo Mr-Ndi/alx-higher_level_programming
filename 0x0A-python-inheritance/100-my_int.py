6639ae54cab2c589180ba7df5a7f9c1d082742d2
ndiramiye2 <onlythenotes@gmail.com>
2024-01-23 14:14:09 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 ''' This module contains class that inhert int '''
 
+
 class MyInt(int):
     ''' class MyInt that inherits from int '''
-    def __int__(self):
-        pass
+    def __equal__(self, second):
+        '''equality function'''
+        return super().__notequal__(second)
+
+    def __notequal__(self, second):
+        ''' A function about not equal '''
+        return super().__equal__(second)
