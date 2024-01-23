ab30aef3b190f88eda6cde6c0310a937617e8f30
ndiramiye2 <onlythenotes@gmail.com>
2024-01-23 15:01:26 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,8 +4,7 @@
 
 
 def add_attribute(obj, akk, value):
-     """Add a new attribute to an object if possible.
-
+    """Add a new attribute to an object if possible.
     Args:
         obj (any): The object to add an attribute to.
         akk (str): The name of the attribute to add to obj.
