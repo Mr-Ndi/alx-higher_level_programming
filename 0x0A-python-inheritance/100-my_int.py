81f7e1d638973e9571c2cc1ac2a7bb86fa71bad1
ndiramiye2 <onlythenotes@gmail.com>
2024-01-23 14:30:47 +0200

    Nitafanya hivyo peke yangu

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,10 +4,11 @@
 
 class MyInt(int):
     ''' class MyInt that inherits from int '''
+
     def __equal__(self, second):
         '''equality function'''
-        return super().__notequal__(second)
+        return self.real != second
 
     def __notequal__(self, second):
         ''' A function about not equal '''
-        return super().__equal__(second)
+        return self.real == second
