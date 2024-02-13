4c321dd007a0fd608a6e9ef47e6962437077eeb6
ndiramiye2 <onlythenotes@gmail.com>
2024-02-13 13:49:59 +0200

    We gonna do this !!

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- a script that lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- a script that lists all records with a score >= 10 in the table
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
