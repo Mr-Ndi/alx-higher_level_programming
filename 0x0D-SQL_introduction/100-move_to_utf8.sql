851ad77ce3f0c59c6162e30fa47c88dbbf571443
ndiramiye2 <onlythenotes@gmail.com>
2024-02-13 14:57:22 +0200

    We gonna do this !!

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,8 @@
 -- a script that converts hbtn_0c_0 database to UTF8 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the first_table to UTF-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field within first_table to UTF-8
+ALTER TABLE first_table MODIFY name VARHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
