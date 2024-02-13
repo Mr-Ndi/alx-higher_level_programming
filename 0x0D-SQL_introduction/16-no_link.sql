be0476b3f31135a9b51546a1a8f6dda55c4a69c9
ndiramiye2 <onlythenotes@gmail.com>
2024-02-13 14:21:03 +0200

    We gonna do this !!

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
--- a script that lists all records of the table second_table of the database
-SELECT sore, name FROM second_table WHERE name IS NOT NULL;
+-- a script that lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
