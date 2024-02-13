001bbe3fb490c7581676b8c75de3ffad7e12a172
ndiramiye2 <onlythenotes@gmail.com>
2024-02-13 13:40:25 +0200

    We gonna do this !!

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score ASC;
+SELECT * FROM second_table ORDER BY score ASC;
