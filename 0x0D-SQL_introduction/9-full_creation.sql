452f622f025b2273c02b39f0567130e210439d74
ndiramiye2 <onlythenotes@gmail.com>
2024-02-13 13:27:04 +0200

    We gonna do this !!

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table called first_table in the current database.
+CREATE TABLE IF NOT EXISTS second_table(id int, name VARCHAR(256), score int);
+INSERT INTO second_table(id, name, score) VALUES(1, "JOHN", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8);
