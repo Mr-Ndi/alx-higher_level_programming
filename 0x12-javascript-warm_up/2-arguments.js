f25707523d438a352b47c342aedae66beb8fa5a4
ndiramiye2 <onlythenotes@gmail.com>
2024-03-11 20:47:55 +0200

    Nitafanya hivyo peke yangu

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (!arg) {
+const arg = process.argv;
+
+if (!arg[2]) {
   console.log('No argument');
-} else {
+} else if (!arg[3]) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
