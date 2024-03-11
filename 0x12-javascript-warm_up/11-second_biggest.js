cedc55a8ce2c6d8048b146363aa22f653401d26c
ndiramiye2 <onlythenotes@gmail.com>
2024-03-11 22:41:59 +0200

    Nitafanya hivyo peke yangu

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,28 +1,9 @@
 #!/usr/bin/node
-
-const nums = process.argv.slice(2);
-
-function secB (nums) {
-  if (nums.length < 2) {
-    return 'Not enough numbers to find the second largest.';
-  }
-
-  let max = parseInt(nums[0]);
-  let max2 = parseInt(nums[1]);
-
-  for (let i = 2; i < nums.length; i++) {
-    const curr = parseInt(nums[i]);
-
-    if (curr > max) {
-      max2 = max;
-      max = curr;
-    } else if (curr > max2 && curr !== max) {
-      max2 = curr;
-    }
-  }
-
-  return max2;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-
-const result = secB(nums);
-console.log(result);
