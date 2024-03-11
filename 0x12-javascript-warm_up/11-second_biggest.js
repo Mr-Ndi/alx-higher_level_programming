31cea0550a347a944afac789a47027d8cbd30ecb
ndiramiye2 <onlythenotes@gmail.com>
2024-03-11 22:39:21 +0200

    Nitafanya hivyo peke yangu

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,20 +3,25 @@
 const nums = process.argv.slice(2);
 
 function secB (nums) {
-  if (!nums[0] || nums[0] === '1' || nums[0] === '0') {
-    return 0;
-  } else {
-    let max = parseInt(nums[0]);
-    let max2 = parseInt(nums[0]);
-    for (let i = 0; i < nums.length; i++) {
-      const curr = parseInt(nums[i]);
-      if (max < curr) {
-        max2 = max;
-        max = curr;
-      }
-    }
-    return max2;
+  if (nums.length < 2) {
+    return 'Not enough numbers to find the second largest.';
   }
+
+  let max = parseInt(nums[0]);
+  let max2 = parseInt(nums[1]);
+
+  for (let i = 2; i < nums.length; i++) {
+    const curr = parseInt(nums[i]);
+
+    if (curr > max) {
+      max2 = max;
+      max = curr;
+    } else if (curr > max2 && curr !== max) {
+      max2 = curr;
+    }
+  }
+
+  return max2;
 }
 
 const result = secB(nums);
