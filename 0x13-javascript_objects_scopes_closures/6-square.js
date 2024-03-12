7c1efcfe2e5b53b8d72a0ea90edffc32623f08c7
ndiramiye2 <onlythenotes@gmail.com>
2024-03-12 21:22:03 +0200

    Nitafanya hivyo peke yangu

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log('c'.repeat(this.width));
+    }
+  }
+};
