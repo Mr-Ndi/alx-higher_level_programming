05d98f1f9347637c00a6e849df7ad0d063f5bfcd
ndiramiye2 <onlythenotes@gmail.com>
2024-03-12 20:11:25 +0200

    Nitafanya hivyo peke yangu

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      this.width = 0;
-      this.height = 0;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
