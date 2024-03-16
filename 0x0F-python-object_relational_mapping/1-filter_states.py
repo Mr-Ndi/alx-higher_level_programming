15d1693c53d6b3756353efc51f60595f914338d9
ndiramiye2 <onlythenotes@gmail.com>
2024-03-16 11:02:39 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""
-importing python3
-"""
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    # connect the db
-    database = MySQLdb.connect(
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-            )
-    # create the cusror && execute the query
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    # executing the query
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # print a tuple
-    [print(data) for data in c.fetchall() if data[1][0] == "N"]
-    # closing the cursor
-    c.close()
-    # closing the db
-    database.close()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
