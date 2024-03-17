5c3509a4749075f356bbd3d4a21a88901b1824ad
ndiramiye2 <onlythenotes@gmail.com>
2024-03-17 12:24:12 +0200

    Nitafanya hivyo peke yangu

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that connects a python script to a database
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from sqlalchemy import  create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    # declaring the vars
+    my_host = 'localhost'
+    users = sys.argv[1]
+    my_password = sys.argv[2]
+    my_db = sys.argv[3]
+    port = 3306
+    
+    engine=create_engine('mysql:{}:{}//@localhost:3306/{}'.format(users,my_password, my_db))
+    sesion = sessionmaker(bind=engine)
+    result= sesion.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    row = result.fetchall()
+    for ans in row:
+        print(ans)
+
+    sesion.close_all()
