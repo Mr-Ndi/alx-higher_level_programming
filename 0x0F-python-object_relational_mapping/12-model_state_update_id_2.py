8e871b83cabafd6f7d47f9bbf141fb1ec2f94cdb
ndiramiye2 <onlythenotes@gmail.com>
2024-03-18 20:19:02 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,7 +6,7 @@ Module that connects a python script to a database
 if __name__ == "__main__":
 
     import sys
-    from sqlalchemy import  create_engine
+    from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 
@@ -16,10 +16,9 @@ if __name__ == "__main__":
     my_password = sys.argv[2]
     my_db = sys.argv[3]
     port = 3306
-    
-    engine=create_engine('mysql://{}:{}@localhost:3306/{}'.format(users,my_password, my_db))
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        users, my_password, my_db))
     session = sessionmaker(bind=engine)
-   
     new_state = "New Mexico"
     state_id = 2
     state = session.query(State).filter(State.id == state_id).first()
