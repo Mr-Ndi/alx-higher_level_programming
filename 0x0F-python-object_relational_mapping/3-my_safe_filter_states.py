d2894dbd0e95e90bdf1a0310b18420752f4c5f9c
ndiramiye2 <onlythenotes@gmail.com>
2024-03-18 16:26:14 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-'''A script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa'''
+'''
+A script that takes in arguments and
+displays all values in the states table of hbtn_0e_0_usa
+'''
 if __name__ == "__main__":
     import sys
     import MySQLdb
 
-    #declaring some variable and initialise them with the arguments
     Cuser = sys.argv[1]
     passw = sys.argv[2]
     ourdb = sys.argv[3]
@@ -12,23 +14,17 @@ if __name__ == "__main__":
     my_p = 3306
     cname = sys.argv[4]
 
-    #establishing the connection
-    ourdb = MySQLdb.connect(host=my_host, user=Cuser, passwd=passw, db=ourdb, port=my_p)
-
-    #creating cursor
+    ourdb = MySQLdb.connect(
+        host=my_host, user=Cuser, passwd=passw, db=ourdb, port=my_p
+        )
     The_cursor = ourdb.cursor()
 
-    #executing the query
-    question="SELECT * from states WHERE name LIKE %s ORDER BY states.id ASC;"
-    The_cursor.execute(question, (cname + '+%',))
-
-    # retriving the answer
+    question = "SELECT * from states WHERE name\
+          LIKE %s ORDER BY states.id ASC;"
+    The_cursor.execute(question, (cname + ' + %',))
     result = The_cursor.fetchall()
 
-    #printing the result
     for city in result:
         print(city)
-
-    #closing corsor And the connection object
     The_cursor.close()
     ourdb.close()
