a87b7e9ffcdb7eff2468709dde9c54f83e068e61
ndiramiye2 <onlythenotes@gmail.com>
2024-03-18 19:55:10 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """
-Module that connects a python script to a database
+importing python
 """
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
 
 if __name__ == "__main__":
 
-    import sys
-    from sqlalchemy import  create_engine
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
+    my_engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]),
+            pool_pre_ping=True)
+    sessione_meker = sessionmaker(bind=my_engine)
+    session = sessione_meker()
 
-    # declaring the vars
-    my_host = 'localhost'
-    users = sys.argv[1]
-    my_password = sys.argv[2]
-    my_db = sys.argv[3]
-    state_name = sys.argv[4]
-    port = 3306
-    
-    engine=create_engine('mysql://{}:{}@localhost:3306/{}'.format(users,my_password, my_db))
-    session = sessionmaker(bind=engine)
-   
-    state = session.query(State).filter(State.name == state_name).first()
-
-
-    if state:
-        print(first_state)
+    for state in session.query(State):
+        if argv[4] == state.name:
+            print("{}".format(state.id))
+            break
     else:
-        print("Not Found")
+        print("Not found")
     session.close()
