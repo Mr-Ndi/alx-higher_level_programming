2f70c240b5c05302665c6e6130572fd09ad52cbb
ndiramiye2 <onlythenotes@gmail.com>
2024-03-18 15:30:22 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     my_cursor = my_db.cursor()
 
     # Execute a SELECT query to fetch data
-    my_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY {} ORDER BY states.id ASC;".format(name))
+    my_cursor.execute("SELECT * FROM states WHERE name\
+                       LIKE BINARY {} ORDER BY states.id ASC;".format(name))
 
     # fetch all the data returned by the query
     my_data = my_cursor.fetchall()
