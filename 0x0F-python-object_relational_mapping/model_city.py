d0cb4d47361b9dde003ccc13e6f0366fbd9f6161
ndiramiye2 <onlythenotes@gmail.com>
2024-03-18 20:35:37 +0200

    Nitafanya hivyo peke yangu

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+
 """
-A python file that contains the class definition of a State and an instance 
+Python3 impporting
 """
-from sqlalchemy import create_engine, PrimaryKeyConstraint, ForeignKey
-from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import Column, Integer, String, ForeignKey, null
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-import sys
+from model_state import Base, State
 
 Base = declarative_base()
 
+
 class City(Base):
-    '''A state definition'''
+    """Defining the City class"""
     __tablename__ = 'cities'
-    state_id = Column(Integer, nullable=False, Foreignkey('states.id'))
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-if __name__=="__main__":
-    engine=create_engine('mysql:{}:{}//@localhost:3306/{}'.format(sys.argv[1],sys.argv[2], sys.argv[3]))
-
-    Base.metadata.create_all(engine)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
