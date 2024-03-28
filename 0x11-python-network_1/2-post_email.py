828f0d46db2cb3ee4dfab0d184d5e11708bedb43
ndiramiye2 <onlythenotes@gmail.com>
2024-03-28 22:55:59 +0200

    Task2_1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to it.
+
+Usage: ./2-post_email.py <URL><Email>
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email" = sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.reade.decode("utf-8"))
