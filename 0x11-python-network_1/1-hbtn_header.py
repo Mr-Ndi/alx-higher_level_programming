b4176d7fcde78fc776a8d80e5446afbff98f555b
ndiramiye2 <onlythenotes@gmail.com>
2024-03-28 21:26:02 +0200

    Nitafanya hivyo peke yangu

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-link = sys.argv[1]
 if __name__ == "__main__":
+    link = sys.argv[1]
     with urllib.request.urlopen(link) as ans:
-        data = ans.getheader('X-REQUEST-ID')
-        print(data)
+        print(dict((ans.header).get('X-REQUEST-ID')))
