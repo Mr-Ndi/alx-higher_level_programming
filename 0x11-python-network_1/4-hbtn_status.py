7f7204bc60feb105a81c90f9cf4378d28da5b414
ndiramiye2 <onlythenotes@gmail.com>
2024-03-28 23:44:54 +0200

    Task_4_5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 if __name__ == "__main__":
-    data = request.ge('https://alx-intranet.hbtn.io/status')
+    data = requests.ge('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(data.text)))
     print("\t- content: {}".format(data.text))
