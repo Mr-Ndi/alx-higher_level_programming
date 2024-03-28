9dc5fdcf5484acaeaec3a0debc7065256d4a33de
ndiramiye2 <onlythenotes@gmail.com>
2024-03-28 21:29:50 +0200

    Nitafanya hivyo peke yangu

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-import urllib.request
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the value of the X-Request-Id
+"""
+from urllib import request
 from sys import argv
 if __name__ == "__main__":
     link = sys.argv[1]
