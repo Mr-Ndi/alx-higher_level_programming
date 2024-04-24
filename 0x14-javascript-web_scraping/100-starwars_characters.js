20101196bd02728316f9b81e5c81a4ca3b609e02
ndiramiye2 <onlythenotes@gmail.com>
2024-04-24 22:35:02 +0200

    Nitafanya hivyo peke yangu

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const { characters } = JSON.parse(body);
+    characters.forEach(characterUrl => {
+      request(characterUrl, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
