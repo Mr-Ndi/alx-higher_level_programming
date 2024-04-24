dd9a6a2594180ef5a3ad2fd4649f9878b1eba555
ndiramiye2 <onlythenotes@gmail.com>
2024-04-24 21:24:54 +0200

    Nitafanya hivyo peke yangu

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,7 @@ request.get(url, (irabu, response, body) => {
   if (irabu) {
     console.log(irabu);
   } else {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
+    const movie_num = (JSON.parse(body)).length();
+    console.log(movie_num);
   }
 });
