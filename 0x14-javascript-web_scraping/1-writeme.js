e5ef456ab344b899c13e57e9cc8aa7ca7ec2f06c
ndiramiye2 <onlythenotes@gmail.com>
2024-04-24 20:33:36 +0200

    Nitafanya hivyo peke yangu

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,15 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const Data = process.argv[3];
 
-if (!filePath) {
-  console.error('Please provide a file path as a command line argument');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, Data, { encoding: 'utf-8' }, (err) => {
+fs.writeFile(filePath, Data, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`'${Data}' has been written to the file ${filePath}`);
   }
 });
