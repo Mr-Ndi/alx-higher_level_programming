4db4a2b4edcbf1fbf33a7625c653a4cbd013528c
ndiramiye2 <onlythenotes@gmail.com>
2024-04-24 19:35:11 +0200

    Nitafanya hivyo peke yangu

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-    console.error('Please provide a file path as a command line argument');
-    process.exit(1);
+  console.error('Please provide a file path as a command line argument');
+  process.exit(1);
 }
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
